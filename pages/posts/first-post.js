e8effd0db69f6fd29b0a24ee99aecade3a4416ee
kurokawa <kurokawa.d@marietta.co.jp>
2021-10-24 20:40:17 +0900

    [Add] Layoutコンポーネントの作成

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,16 @@
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 };
 
