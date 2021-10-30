c3c02b0af39bd76c1d0c5cf896425710c4c939dc
kurokawa <kurokawa.d@marietta.co.jp>
2021-10-31 00:00:26 +0900

    [Add] test

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
-  console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+  console.log('test');
   return (
     <Layout home>
       <Head>
